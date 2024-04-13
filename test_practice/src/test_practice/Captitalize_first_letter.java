39d30a838635136fbf395d0236e2ef9f2c3407c5
niraj <niraj@DESKTOP-UI77LPV>
2024-04-13 12:45:14 +0530

    new program

--- test_practice/src/test_practice/Captitalize_first_letter.java
+++ test_practice/src/test_practice/Captitalize_first_letter.java
@@ -1,8 +1,15 @@
 package test_practice;
 
 public class Captitalize_first_letter {
-
-	public static void main(String[] args) {
+	
+	
+	
+public static void main(String[] args) {
+	//abc();
+	def();
+}
+	 public static void abc() {
+		
 		String str="i have a car";
 		String[]x=str.split(" ");
 		String temp=" ";
@@ -11,6 +18,16 @@ public class Captitalize_first_letter {
 			
 		}System.out.println(temp);
 
+	}
+	  public static void def() {
+		String str= "thisisjava";
+		String[]x=str.split(" ");
+		String temp=" ";
+		for(int i=0; i<=x.length; i++) {
+			temp= temp+" "+x[i].replace(x[i].charAt(0),Character.toUpperCase(x[i].charAt(0)));
+		
+		}System.out.println(temp);
+		
 	}
 
 }
